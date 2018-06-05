7856e017e14e8e58612a7664fce1bb3635341165
Dia Saleh <dia@Noors-MacBook-Pro.local>
2018-06-05 16:59:12 +0300

    changing view response

--- newapp/views.py
+++ newapp/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index!")
+    return HttpResponse("Hello from changing_view_response Branch!")
 
